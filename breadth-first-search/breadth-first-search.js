ce7f2277847d9394b710ab91928320a8762793b2
Thiago Nogueira <thiago.droz@gmail.com>
2017-12-05 21:31:26 -0200

    Breadth first search

---
+++ breadth-first-search/breadth-first-search.js
@@ -0,0 +1,23 @@
+const Vertex = require('./bfs-vertex');
+
+const breadthFirstSearch = (root) => {
+  let queue = [];
+  root.visited = true;
+
+  queue.push(root);
+
+  while (queue.length > 0) {
+    let actualVertex = queue[0];
+    queue.shift();
+    console.log(actualVertex.data);
+
+    actualVertex.neighbours.forEach (v => {
+      if (!v.visited) {
+        v.visited = true;
+        queue.push(v);
+      }
+    });
+  }
+};
+
+module.exports = breadthFirstSearch;
---
+++ breadth-first-search/breadth-first-search.test.js
@@ -0,0 +1,49 @@
+const Vertex = require('./bfs-vertex');
+const bfsFunctions = [
+  require('./breadth-first-search')
+];
+
+let root = null;
+
+bfsFunctions.forEach(bfs => {
+  test('bfs function is defined', () => {
+    expect(bfs).toBeDefined();
+  });
+
+  test('Calling bfs with a graph with `n` nodes prints out `n` statements', () => {
+    bfs(root);
+
+    expect(console.log.mock.calls.length).toEqual(5);
+  });
+
+  test('Calling bfs with a graph prints out the correct values', () => {
+    bfs(root);
+
+    expect(console.log.mock.calls[0][0]).toEqual(1);
+    expect(console.log.mock.calls[1][0]).toEqual(2);
+    expect(console.log.mock.calls[2][0]).toEqual(4);
+    expect(console.log.mock.calls[3][0]).toEqual(3);
+    expect(console.log.mock.calls[4][0]).toEqual(5);
+  });
+
+  beforeEach(() => {
+    jest.spyOn(console, 'log');
+
+    let vertex1 = new Vertex(1);
+    let vertex2 = new Vertex(2);
+    let vertex3 = new Vertex(3);
+    let vertex4 = new Vertex(4);
+    let vertex5 = new Vertex(5);
+    
+    vertex1.neighbours.push(vertex2);
+    vertex1.neighbours.push(vertex4);
+    vertex4.neighbours.push(vertex5);
+    vertex2.neighbours.push(vertex3);
+    
+    root = vertex1;
+  });
+
+  afterEach(() => {
+    console.log.mockRestore();
+  });
+});
